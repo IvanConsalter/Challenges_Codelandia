fcd5d62022727631831512ab182867a4d0cf5a39
Ivan Consalter <ivanconsalter1@gmail.com>
2023-07-23 15:35:09 -0300

    update to angular 12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BlogComponent } from './challenges/blog/blog.component';
-import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { JordanShoesComponent } from './challenges/jordan-shoes/jordan-shoes.component';
 import { OnePageComponent } from './challenges/one-page/one-page.component';
 import { LoginComponent } from './challenges/login/login.component';
 import { ChihiroComponent } from './challenges/chihiro/chihiro.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -30,9 +30,9 @@ import { ChihiroComponent } from './challenges/chihiro/chihiro.component';
     AppRoutingModule,
     HttpClientModule,
 
-    FontAwesomeModule,
+    CoreModule,
 
-    CoreModule
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FooterComponent } from "./footer/footer.component";
 import { NavbarComponent } from "./navbar/navbar.component";
 import { CommonModule } from "@angular/common";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { RouterModule } from "@angular/router";
 
 @NgModule({
@@ -12,9 +11,7 @@ import { RouterModule } from "@angular/router";
   ],
   imports: [
     CommonModule,
-    RouterModule,
-
-    FontAwesomeModule
+    RouterModule
   ],
   exports: [
     NavbarComponent,
